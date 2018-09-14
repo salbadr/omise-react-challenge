4b6a33ae04505907d8d4b99cae19c829cc68aa6b
sbadr <salman.badr@achieve3000.com>
2018-09-14 14:02:50 -0400

    Added test to add 1 child to parent

--- src/tests/Formatter.test.js
+++ src/tests/Formatter.test.js
@@ -8,8 +8,26 @@ describe('Formatter', () => {
 
         });
 
-
         return output;
+    }
+
+    function getChildren(input) {
+        return input.reduce((parents, value) => {
+            return value.reduce((parents, item) => {
+                if (!item.parent_id) {
+                    parents.push(item);
+                }
+                else {
+                    parents.forEach((parent, index) => {
+                        if (item.parent_id === parent.id) {
+                            parents[index].children.push(item);
+                        }
+                        return true;
+                    });
+                }
+                return parents;
+            }, parents);
+        }, []);
     }
 
     it('should extract all root level items', () => {
@@ -45,7 +63,6 @@ describe('Formatter', () => {
 
         expect(result.length).toBe(1);
         expect(result[0].length).toBe(3);
-
 
 
     });
@@ -84,7 +101,6 @@ describe('Formatter', () => {
         expect(result[0].length).toBe(1);
 
 
-
     });
     it('should extract all root level items and children items', () => {
         const input = {
@@ -97,21 +113,27 @@ describe('Formatter', () => {
                     "parent_id": null
                 }],
             "1":
-                [{"id": 12,
+                [{
+                    "id": 12,
                     "title": "Red Roof",
                     "level": 1,
                     "children": [],
-                    "parent_id": 10},
-                    {"id": 18,
+                    "parent_id": 10
+                },
+                    {
+                        "id": 18,
                         "title": "Blue Roof",
                         "level": 1,
                         "children": [],
-                        "parent_id": 10},
-                    {"id": 13,
+                        "parent_id": 10
+                    },
+                    {
+                        "id": 13,
                         "title": "Wall",
                         "level": 1,
                         "children": [],
-                        "parent_id": 10}],
+                        "parent_id": 10
+                    }],
         };
 
 
@@ -120,7 +142,6 @@ describe('Formatter', () => {
         expect(result.length).toBe(2);
         expect(result[0].length).toBe(1);
         expect(result[1].length).toBe(3);
-
 
 
     });
@@ -135,21 +156,27 @@ describe('Formatter', () => {
                     "parent_id": null
                 }],
             "1":
-                [{"id": 12,
+                [{
+                    "id": 12,
                     "title": "Red Roof",
                     "level": 1,
                     "children": [],
-                    "parent_id": 10},
-                    {"id": 18,
+                    "parent_id": 10
+                },
+                    {
+                        "id": 18,
                         "title": "Blue Roof",
                         "level": 1,
                         "children": [],
-                        "parent_id": 10},
-                    {"id": 13,
+                        "parent_id": 10
+                    },
+                    {
+                        "id": 13,
                         "title": "Wall",
                         "level": 3,
                         "children": [],
-                        "parent_id": 10}],
+                        "parent_id": 10
+                    }],
         };
 
 
@@ -160,7 +187,43 @@ describe('Formatter', () => {
         expect(result[1].length).toBe(2);
 
 
+    });
+    it('should add children to the correct parent when only 1 child', () => {
+        const input = {
+            "0":
+                [{
+                    "id": 10,
+                    "title": "House",
+                    "level": 0,
+                    "children": [],
+                    "parent_id": null
+                },
+                    {
+                        "id": 13,
+                        "title": "House",
+                        "level": 0,
+                        "children": [],
+                        "parent_id": null
+                    }],
+            "1":
+                [{
+                    "id": 12,
+                    "title": "Red Roof",
+                    "level": 1,
+                    "children": [],
+                    "parent_id": 10
+                }],
+        };
+
+        const result = getChildren(Formatter(input));
+
+        expect(result[0].children[0].id).toBe(12);
+        expect(result[1].children.length).toBe(0);
+
+
 
     });
+
+
 
 });
